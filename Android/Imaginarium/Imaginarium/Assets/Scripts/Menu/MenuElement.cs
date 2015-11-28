2290d4a64055bc6a148d3120680b6b12bd547799
Nikolay Akopov <asoman97@gmail.com>
2015-11-29 03:53:46 +0400

    Продолжаю реализацию меню. Столкнулся с неожиданными трудностями с canvas. Дело затянулось.

---
+++ Android/Imaginarium/Imaginarium/Assets/Scripts/Menu/MenuElement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public class MenuElement
+{
+        private GameObject _element;
+        private Vector3 _pos;
+        private Vector3 _startPos;
+
+        public MenuElement(GameObject element) {
+        _element = element;
+        _startPos = _element.transform.position;
+        _pos = _startPos;
+        }
+
+    public void MoveTo(Vector3 destination)
+    {
+        _pos = destination;
+    }
+
+    public void MoveToStart() {
+        _pos = _startPos;
+    }
+
+    public void Update() {
+        _element.transform.position = Vector3.Lerp(_element.transform.position, _pos, Time.deltaTime);
+    }
+
+}
---
+++ Android/Imaginarium/Imaginarium/Assets/Scripts/Menu/MenuHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour {
+
+    private MenuElement Title;
+    private MenuElement StartButton;
+    private MenuElement SearchButton;
+
+    private Vector3 CenterPos;
+
+    // Use this for initialization
+    void Start () {
+        Title = new MenuElement(gameObject.transform.FindChild("Title").gameObject);
+        StartButton = new MenuElement(gameObject.transform.FindChild("StartButton").gameObject);
+        SearchButton = new MenuElement(gameObject.transform.FindChild("StartButton").gameObject);
+        CenterPos = gameObject.transform.FindChild("Center").position;
+
+        Title.MoveTo(CenterPos);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Title.Update();
+        StartButton.Update();
+        SearchButton.Update();
+	}
+
+    public void OpenStartButtonScreen() {
+        Title.MoveToStart();
+        SearchButton.MoveToStart();
+        StartButton.MoveTo(CenterPos);
+    }
+
+    public void OpenSearchButtonScreen()
+    {
+        Title.MoveToStart();
+        StartButton.MoveToStart();
+        SearchButton.MoveTo(CenterPos);
+    }
+
+    public void OpenTitleScreen()
+    {
+        SearchButton.MoveToStart();
+        StartButton.MoveToStart();
+        Title.MoveTo(CenterPos);
+    }
+
+}
