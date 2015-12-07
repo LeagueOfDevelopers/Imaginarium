b99afe2813dc25159bbca3f2d3f769c0636cefd2
Nikolay Akopov <asoman97@gmail.com>
2015-12-07 15:55:08 +0300

    Продолжение тестов WWW

--- Android/Imaginarium/Imaginarium/Assets/Scripts/TestingWWW.cs
+++ Android/Imaginarium/Imaginarium/Assets/Scripts/TestingWWW.cs
@@ -4,7 +4,8 @@ using UnityEngine.UI;
 
 public class TestingWWW : MonoBehaviour
 {
-    public string url = "http://185.87.49.245:8080/ImagServerApp_war/JoinLobby";
+    //public string url = "http://185.87.49.245:8080/ImagServerApp_war/JoinLobby";
+    public string url = "http://google.com";
     IEnumerator Start()
     {
         WWWForm form = new WWWForm();
@@ -12,8 +13,9 @@ public class TestingWWW : MonoBehaviour
         WWW www = new WWW(url,form);
         yield return www;
          Debug.Log(www.text.ToString());
-        JSONObject json = new JSONObject(www.text.ToString());
+        //JSONObject json = new JSONObject(www.text.ToString());
         Text text = GetComponent<Text>();
-        text.text = json.GetField("token").ToString();
+        text.text = www.text.ToString();
+        //text.text = json.GetField("token").ToString();
     }
 }
