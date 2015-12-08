2705e36e8e2c2a03b79e0e3c53ca1523ab05ce9d
Nikolay Akopov <asoman97@gmail.com>
2015-12-08 23:22:11 +0300

    Начала реализация логики меню.

--- Android/Imaginarium/Imaginarium/Assets/Scripts/Menu/MenuLogic.cs
+++ Android/Imaginarium/Imaginarium/Assets/Scripts/Menu/MenuLogic.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class MenuLogic : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+
+    private enum Stage {Idle, WaitingForToken, SerachingGame }
+    private Stage currentStage = Stage.Idle;
+    ServerDriver driver = new ServerDriver();
+    Prefs prefs = new Prefs();
+
 	// Update is called once per frame
 	void Update () {
-	
+        switch (currentStage)
+        {
+            case Stage.WaitingForToken:
+                if (driver.isDone())
+                {
+                    string token = driver.getResponse().GetField("token").ToString();
+                    Debug.Log(token);
+                    currentStage = Stage.SerachingGame;
+                }
+                break;
+            case Stage.SerachingGame:
+                
+                break;
+        }
 	}
+
+    public void StartSearch() {
+        driver.JoinLobby();
+        currentStage = Stage.WaitingForToken;
+    }
+
+    public void SetIdle() {
+        currentStage = Stage.Idle;
+    }
 }
