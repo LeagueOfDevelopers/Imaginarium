1050a24043e8926b5ccf3a0e3325669f35f4b82a
Nikolay Akopov <asoman97@gmail.com>
2015-12-30 20:18:50 +0300

    4 Стадия. Осталось глобальное тестирование и улучшение графической состовляющей. Альфа готова нахуй.

---
+++ Android/Imaginarium/Imaginarium/Assets/Scripts/Game/StageLogic/FourthStageLogic.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FourthStageLogic : MonoBehaviour {
+
+    public GameObject Cards;
+    Prefs prefs = new Prefs();
+
+    // Use this for initialization
+    void Start () {
+        SetCards();
+        ShowVoteForCards();
+    }
+
+    private void SetCards()
+    {
+        Cards.GetComponent<CardNamer>().SetCards(prefs.getChosenCards());
+    }
+
+    public void OKButtonClickEventHandler()
+    {
+        prefs.setIsStageComplete(true);
+        SceneManager.LoadScene("Game");
+    }
+
+    private void ShowVoteForCards()
+    {
+        int[] votes = prefs.getVoteForCards();
+
+        int i = 0;
+        foreach (int vote in votes)
+        {
+            GameObject child = Cards.transform.FindChild("Card" + i).gameObject;
+            GameObject text = child.transform.GetChild(0).gameObject;
+            text.GetComponent<Text>().text = vote.ToString();
+            if (vote == prefs.getHeadCard())
+                text.GetComponent<Text>().color = Color.red;
+            if(vote == prefs.getOwnCard())
+                text.GetComponent<Text>().color = Color.green;
+            i++;
+        }
+    }
+}
