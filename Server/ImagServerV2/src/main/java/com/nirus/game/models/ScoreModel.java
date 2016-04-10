2605c6e035db8441a31c81db282afba6598dae4a
ndiezel <ndiezel0@gmail.com>
2016-04-10 06:25:11 +0300

    Lobby, Scores and First Stage are in new format.

---
+++ Server/ImagServerV2/src/main/java/com/nirus/game/models/ScoreModel.java
@@ -0,0 +1,10 @@
+package com.nirus.game.models;
+
+import com.nirus.game.models.basics.PlayerModel;
+
+/**
+ * Created by ndiezel on 10.04.2016.
+ */
+public class ScoreModel {
+    public PlayerModel[] players;
+}
---
+++ Server/ImagServerV2/src/main/java/com/nirus/models/LobbyModel.java
@@ -0,0 +1,12 @@
+package com.nirus.models;
+
+import com.nirus.game.models.basics.PlayerModel;
+
+/**
+ * Created by ndiezel on 10.04.2016.
+ */
+public class LobbyModel {
+    public String status;
+    public String token;
+    public PlayerModel[] players;
+}
